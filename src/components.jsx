c52a21cb2a38da15ecf65493e129d9720b855fba
chlloehung <144968987+chlloehung@users.noreply.github.com>
2025-04-15 13:53:51 +0000

    fahrenheit question

--- src/components.jsx
+++ src/components.jsx
@@ -1,13 +1,23 @@
-import {hello} from './functions.js';
+import {fahrenheitToCelsius, hello} from './functions.js';
 
 function Question1(){
     return <section>
 1. Write a function that takes a _name_ as an argument and _returns a string_ that _contains "hello"_ and the name paramerer. Test at least 3 names. <br />*Note ... I solved this one already
       <h2>results</h2>
-      <p>hello("Rich") == "{hello('Rich')}"</p>
-      <p>hello("Bill") == "{hello('Bill')}"</p>
-      <p>hello("Chris") == "{hello('Chris')}"</p>
+      <p>hello("Chloe") == "{hello('Chloe')}"</p>
+      <p>hello("Kaitlyn") == "{hello('Kaitlyn')}"</p>
+      <p>hello("Jose") == "{hello('Jose')}"</p>
     </section>;
 }
 
-export {Question1}
+function Question2(){
+    return <section>
+2. Write a function that converts from fahrenheit to celsius 
+<h2>results</h2>
+<p>fahrenheitToCelsius(32) == "{fahrenheitToCelsius(32)}"</p>
+<p>fahrenheitToCelsius(212) == "{fahrenheitToCelsius(212)}"</p>
+<p>fahrenheitToCelsius(70) == "{fahrenheitToCelsius(70)}"</p>
+    </section>;
+}
+
+export {Question1, Question2}
